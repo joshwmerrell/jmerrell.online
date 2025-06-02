292657613af17a53162c2ff9ca101be1ef784c5f
joshwmerrell <joshwmerrell@gmail.com>
2025-06-02 11:16:12 -0600

    Update jmerrells_you_choose_game.py

--- python_projects/jmerrells_you_choose_game.py
+++ python_projects/jmerrells_you_choose_game.py
@@ -72,13 +72,13 @@ def pleasechoose4() :
 # First, we want to determine what OS our player is running so that we can clear and write in the terminal of the OS properly for each senario.
 
 import os
-import .setup_values
+import setup_values
 
 
-windows = .setup_values.os == windows
-linux = .setup_values.os == linux
-macos = .setup_values.os == macos
-other = .setup_values.os == other
+windows = setup_values.os == windows
+linux = setup_values.os == linux
+macos = setup_values.os == macos
+other = setup_values.os == other
 
 if windows :
     def clear() :
